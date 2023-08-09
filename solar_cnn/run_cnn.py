998ab40f9ba3fea57be478ebb4d170bf05cae8a0
porter <porterbmoody@gmail.com>
2023-08-09 13:34:37 -0600

    f

--- solar_cnn/run_cnn.py
+++ solar_cnn/run_cnn.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#%%
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-
-
-
-def create_cnn_model(input_shape, num_classes):
-	model = Sequential()
-	model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-
-	model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-
-	model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-
-	model.add(Flatten())
-	model.add(Dense(256, activation='relu'))
-	model.add(Dense(num_classes, activation='sigmoid'))  # Output layer for coordinates
-	return model
-
-create_cnn_model()
-
-
-
-=======
->>>>>>> 35dc01fdc4b1d4912f726c238157db3aa87be3d0
 #%%
 from PIL import Image, ImageDraw
 import numpy as np
@@ -57,27 +27,7 @@ train_data = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
-<<<<<<< HEAD
-#%%
-coordinates = [
-	(160, 400-145),
-	(128, 400-278),
-	(276, 400-277),
-	(276, 400-148),
-	]
-coordinates_list = [
-	160, 400-145,
-	128, 400-278,
-	276, 400-277,
-	276, 400-148,
-	]
-draw = ImageDraw.Draw(image)
-draw.polygon(coordinates, outline = 'black', width = 1)
-image
-coordinates_list
-=======
 train_data
->>>>>>> 35dc01fdc4b1d4912f726c238157db3aa87be3d0
 
 #%%
 
@@ -90,18 +40,6 @@ test_data = tf.keras.utils.image_dataset_from_directory(
   batch_size=batch_size)
 test_data
 
-<<<<<<< HEAD
-model = tf.keras.models.Sequential([
-	# tf.keras.layers.experimental.preprocessing.Rescaling(1./255, input_shape=(2, 2, 1)),
-	# tf.keras.layers.Flatten(),
-	tf.keras.layers.Dense(8, input_shape = [1]),
-	tf.keras.layers.Dense(1),
-])
-
-# Compile the model
-model.compile(optimizer='sgd',
-			  loss='mean_squared_error')
-=======
 # #%%
 # model.compile(
 #   optimizer='adam',
@@ -151,25 +89,10 @@ loss = 'mean_squared_error'
 optimizer = 'sgd'
 metrics=['accuracy']
 model.compile(optimizer=optimizer, loss=loss, metrics = metrics)
->>>>>>> 35dc01fdc4b1d4912f726c238157db3aa87be3d0
 
 # Print the model summary
 model.summary()
 
-<<<<<<< HEAD
-# y = np.array([200,200])
-# list(sum(coordinates, ()))
-# %%
-# x = np.array([1,2,3,4,5,6,7])
-x = np.array([1,2,3,4, 5, 6, 7])
-y = 2*x
-y
-data = pd.DataFrame({
-	'x':x,
-	'y':y,
-})
-data
-=======
 #%%
 epochs=10
 model.fit(
@@ -177,7 +100,6 @@ model.fit(
   validation_data=test_data,
   epochs = epochs
 )
->>>>>>> 35dc01fdc4b1d4912f726c238157db3aa87be3d0
 
 #%%
 # Evaluate the model on the test dataset
@@ -244,14 +166,9 @@ print(x)
 print(y)
 
 #%%
-<<<<<<< HEAD
-model = tf.keras.models.Sequential([
-	tf.keras.layers.Dense(1, input_shape=[1]),
-=======
 x = tf.stack(image_array)
 y = np.array([
     [10]
->>>>>>> 35dc01fdc4b1d4912f726c238157db3aa87be3d0
 ])
 x
 y
